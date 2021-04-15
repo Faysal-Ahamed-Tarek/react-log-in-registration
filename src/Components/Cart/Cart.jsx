2527fc9c326be1bdb65b7db26ce175ac732a3704
Faysal-Ahamed-Tarek <faysal.ahamed.tarek1@gmail.com>
2021-04-15 01:32:45 -0700

    cart updated

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
-
+import { React , useEffect, useState } from 'react';
 import './cart.css';
 import Product_listing from './ProductList';
 
 const Cart =() => {
+
     //add to cart state
     const [Cart_Info,setCart_Info] = useState(() => {
         const DefaultProduct = localStorage.getItem("set_cart_product");
         return JSON.parse(DefaultProduct);
     });
+
     //remove item from cart
     const RemoveProduct = (removeItem) => {
         console.log(removeItem);
@@ -18,15 +19,19 @@ const Cart =() => {
     useEffect( () => {
         localStorage.setItem("set_cart_product",JSON.stringify(Cart_Info));
     },[Cart_Info]);
+
     //quantity
     const [Product_Quantity,setProduct_Quantity] = useState(1);
+
     //increment quantity
     const Increment = () =>{
-        
+        setProduct_Quantity(Product_Quantity + 1);
     }
     //increment quantity
     const Decrement = () =>{
-
+        if(Product_Quantity > 1 ){
+            setProduct_Quantity(Product_Quantity - 1);
+        }
     }
     return(
         <div className="container-fluid pt-5 pb-3">
@@ -61,7 +66,6 @@ const Cart =() => {
                         <div className="Cart_calculate pt-2 pb-3">
                             <h5 className="text-center">Order Summery</h5 >
                         </div>
-
                     </div>
                 </div>
             </div>
